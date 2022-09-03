9bab879ae707dd297bb17003ebb60c5112eb26af
milena-mognon <milenamognon@gmail.com>
2022-09-03 18:03:59 -0300

    feat: styles and page history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,62 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <tr>
+            <th>Tarefas</th>
+            <th>Duração</th>
+            <th>Inícios</th>
+            <th>Status</th>
+          </tr>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Cancelado</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em Andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,7 +53,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton type="submit">
           <Play size="24" />
           Começar
         </StartCountdownButton>
