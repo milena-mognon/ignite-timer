d4a860d23fd6b131daaee408c24d47490b21274f
milena-mognon <milenamognon@gmail.com>
2022-09-04 11:43:21 -0300

    feat: create new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,16 +14,60 @@ import {
   StopCountdownButton,
 } from './styles';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+import { string } from 'zod/lib';
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+});
+
+// consigo a tipagem usando a validação do formulário feita com zod
+type NewCicleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
 
-  function handleCreateNewCycle(data: any) {
-    console.log(data);
+  const { register, handleSubmit, watch, formState, reset } =
+    useForm<NewCicleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: '',
+        minutesAmount: 0,
+      },
+    });
+
+  function handleCreateNewCycle(data: NewCicleFormData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((state) => [...state, newCycle]); // state é o estado atual de cycles, antes do set
+    setActiveCycleId(newCycle.id);
+    reset(); // volta para os valores default
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  // console.log(formState.errors);  consegue ver os erros
 
   const task = watch('task');
   const isSubmitDisabled = !task;
+
+  console.log(activeCycle);
 
   return (
     <HomeContainer>
